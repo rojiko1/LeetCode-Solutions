a3993d864b4dff348fda4c4350a9a273a6eb8336
rojiko1 <rkadekar12@outlook.com>
2022-10-07 22:58:29 -0700

    problems 3, 5, 15, 43, 58, 66, 67, 69, 70, 83, 88, 94, 100, 101, 206

--- src/Workspace/ListNode.java
+++ src/Workspace/ListNode.java
@@ -1,3 +1,5 @@
+package Workspace;
+
 public class ListNode {
     int val;
     ListNode next;
---
+++ src/Workspace/Solution.java
@@ -0,0 +1,10 @@
+package Workspace;
+// this is a workspace
+
+class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+
+}
